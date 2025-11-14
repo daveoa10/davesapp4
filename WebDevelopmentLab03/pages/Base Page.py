544f06baf23cb95b27ec4079989c6c55e3ff23ff
daveoa10 <damidave7@outlook.com>
2025-11-13 22:35:18 -0500

    Add files via upload

---
+++ WebDevelopmentLab03/pages/Base Page.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import mock_requests as requests
+st.write('Hi')
+def languageCapitals(language):
+    alist = []
+    res = requests.get(f'https://restcountries.com/v3.1/lang/{language}')
+    if res.status_code == 200:
+        print('Success!')
+    variable = res.json()
+    for i in range(len(variable)):
+        if variable[i]['area'] > 1000000:
+            alist.append(variable[i]['capital'][0])
+    return sorted(alist)
+st.write(languageCapitals('English'))
